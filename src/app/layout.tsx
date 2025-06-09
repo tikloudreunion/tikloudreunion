a86e5d7e240292b95c5dc201fd6e9bc38b078b64
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-06-09 11:12:57 +0400

    Feat: Add the dashboard template of dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/app/globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export const metadata: Metadata = {
   title: "Ti Kloud Réunion",
@@ -13,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
       <body>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Dashboard from '@/dashboard/Dashboard';
+
 export default function Page() {
   return (
-    <></>
+    <>
+      <Dashboard />
+    </>
   );
 }
